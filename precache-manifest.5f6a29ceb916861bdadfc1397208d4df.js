7271a7edd9c82cc02fe22dc8e540de5842c7ee49
Nicholas McKenzie <nicholas.a.mckenzie@gmail.com>
2019-12-03 00:10:19 -0500

    Updates

---
+++ precache-manifest.5f6a29ceb916861bdadfc1397208d4df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72b35ae53d21ab78d820ad32e546d086",
+    "url": "/gh-pages-app/index.html"
+  },
+  {
+    "revision": "bf19ba7966e73fb98ac8",
+    "url": "/gh-pages-app/static/css/main.b148a987.chunk.css"
+  },
+  {
+    "revision": "d461d9d15decc912a3ee",
+    "url": "/gh-pages-app/static/js/2.febd1b74.chunk.js"
+  },
+  {
+    "revision": "bf19ba7966e73fb98ac8",
+    "url": "/gh-pages-app/static/js/main.48aa0274.chunk.js"
+  },
+  {
+    "revision": "a8073d3b0e04449e313b",
+    "url": "/gh-pages-app/static/js/runtime-main.18d2324b.js"
+  },
+  {
+    "revision": "6e11f2159d80948e19002a0dd93d2c50",
+    "url": "/gh-pages-app/static/media/welcomeScreenImage.6e11f215.png"
+  }
+]);
